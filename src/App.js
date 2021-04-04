9a563aeb62c358cf9fb6f65e2d3a7ea58d9ddfec
Blessy Johnson <blessy.johnson@tcs.com>
2021-04-04 17:21:44 +0530

    created new project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './Components/Homepage/Homepage.component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am  a new react app</h1>
+     <Homepage/>
+    </div>
+  );
+}
+
+export default App;
