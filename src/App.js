a42778659f36e342c9392513e2c710537565bd3e
Blessy Johnson <blessy.johnson@tcs.com>
2021-04-04 21:10:14 +0530

    added menuitems

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from './Components/Homepage/Homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 import './App.css';
 
 function App() {
